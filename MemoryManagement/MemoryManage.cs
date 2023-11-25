3861579d23e6f74c34bd0d6eaee7d8b9a7937325
Alylaxy <jp8nogueira@gmail.com>
2023-11-25 14:58:15 -0300

    Final Project. Now replacing processes

--- MemoryManagement/MemoryManage.cs
+++ MemoryManagement/MemoryManage.cs
@@ -7,22 +7,106 @@ namespace MemoryManagement
     {
         public static List<int> MemoryBlock = Enumerable.Repeat(0, 64).ToList();
 
-        public static int[] ProcessSize { get; set; }
+        public static List<int> ProcessSize { get; set; }
 
-        public static int ProcessLength { get; }
+        public static List<Tuple<int, int>> Processos = new List<Tuple<int, int>>();
+
         static MemoryManage()
         {
-            ProcessSize = new int[] { 10, 6, 15, 80 };
-            ProcessLength = ProcessSize.Length;
+            ProcessSize = new List<int> { 10, 6, 15, 80 };
 
             MemoryBlock[9] = 1;
             MemoryBlock[21] = 1;
+        }
+
+        public static void FirstFit()
+        {
+            foreach (int processSize in ProcessSize)
+            {
+                if (processSize > 64)
+                {
+                    Console.WriteLine("Processo maior do que capacidade total da memória.\nConsidere alterar seu tamanho nas configurações.");
+                    continue;
+                }
+                int bestIndex = -1;
+
+                for (int i = 0; i < MemoryBlock.Count - 1; i++)
+                {
+                    if (MemoryBlock[i] == 0) // Start of a free block
+                    {
+                        int j;
+                        for (j = i; j < MemoryBlock.Count - 1; j++)
+                        {
+                            if (MemoryBlock[j] == 1) // End of a free block
+                            {
+                                break;
+                            }
+                        }
+                        int size = j - i;
+                        if (size >= processSize)
+                        {
+                            bestIndex = i;
+                            break; // Found the first suitable free block
+                        }
+                        i = j; // Skip to the end of the current free block
+                    }
+                }
+                Console.WriteLine($"Reg. Base = {bestIndex}\tReg. Limite = {bestIndex + processSize}\nTamanho Processo: {processSize}");
+                if (bestIndex != -1)
+                {
+                    // Add process to the tuple
+                    Processos.Add(new Tuple<int, int>(bestIndex, bestIndex + processSize));
+
+                    // Allocate the process to the first free block
+                    for (int i = bestIndex; i < bestIndex + processSize; i++)
+                    {
+                        MemoryBlock[i] = 1;
+                    }
+                }
+                else
+                {
+                    // Find best fit after considering removing processes
+                    int bestProcess = int.MaxValue;
+                    for (int i = 0; i < Processos.Count - 1; i++)
+                    {
+                        Tuple<int, int>? processo = Processos[i];
+                        var tamanho = processo.Item2 - processo.Item1;
+                        if (tamanho >= processSize)
+                        {
+                            bestProcess = i;
+                            break;
+                        }
+                    }
+
+                    var inicio = Processos[bestProcess].Item1;
+                    var fim = inicio + processSize;
+
+                    //Remove previous process, and adds this new process on it's place
+                    for (int i = Processos[bestProcess].Item1; i <= Processos[bestProcess].Item2; i++)
+                    {
+
+                        // Remove exceeding space from previous process
+                        MemoryBlock[i] = i > fim ? 0 : 1;
+
+                        // Remove previous process from tuple
+                        Processos.RemoveAt(bestProcess);
+
+                        // Add new process on tuple
+                        Processos.Add(new Tuple<int, int>(inicio, fim));
+                    }
+                }
+            }
         }
 
         public static void BestFit()
         {
             foreach (int processSize in ProcessSize)
             {
+                if (processSize > 64)
+                {
+                    Console.WriteLine("Processo maior do que capacidade total da memória.\nConsidere alterar seu tamanho nas configurações");
+                    continue;
+                }
                 int bestIndex = -1;
                 int bestSize = int.MaxValue;
 
@@ -50,6 +134,18 @@ namespace MemoryManagement
                 Console.WriteLine($"Reg. Base = {bestIndex}\tReg. Limite = {bestIndex + processSize}\nTamanho Processo: {processSize}");
                 if (bestIndex != -1)
                 {
+                    // Add process to the tuple
+                    Processos.Add(new Tuple<int, int>(bestIndex, processSize));
+
+                    // Save the position of this process for future removal
+                    var inicio = bestIndex;
+                    var fim = bestIndex + processSize;
+
+                    if (Processos != null)
+                    {
+                        Processos.Add(new Tuple<int, int>(inicio, fim));
+                    }
+
                     // Allocate the processSize to the best free block
                     for (int i = bestIndex; i < bestIndex + processSize; i++)
                     {
@@ -58,7 +154,35 @@ namespace MemoryManagement
                 }
                 else
                 {
-                    Console.WriteLine("Memória insuficiente para conter o processo.");
+
+                    // Find best fit after considering removing processes
+                    int bestProcess = int.MaxValue;
+                    for (int i = 0; i < Processos.Count; i++)
+                    {
+                        Tuple<int, int>? processo = Processos[i];
+                        var tamanho = processo.Item2 - processo.Item1;
+                        if(tamanho > processSize && tamanho < bestProcess)
+                        {
+                            bestProcess = i;
+                        }
+                    }
+
+                    var inicio = Processos[bestProcess].Item1;
+                    var fim = inicio + processSize;
+
+                    //Remove previous process, and adds this new process on it's place
+                    for (int i = Processos[bestProcess].Item1; i <= Processos[bestProcess].Item2; i++)
+                    {
+                        
+                        // Remove exceeding space from previous process
+                        MemoryBlock[i] = i > fim ? 0 : 1;
+
+                        // Remove previous process from tuple
+                        Processos.RemoveAt(bestProcess);
+
+                        // Add new process on tuple
+                        Processos.Add(new Tuple<int, int>(inicio, fim));
+                    }
                 }
             }
             int k = 1;
@@ -66,54 +190,9 @@ namespace MemoryManagement
             {
                 Console.WriteLine($"Bloco {k}:\t{block}");
                 k++;
-            }
-
-            Console.ReadLine();
-        }
-
-        public static void FirstFit()
-        {
-            foreach (int processSize in ProcessSize)
-            {
-                int bestIndex = -1;
-
-                for (int i = 0; i < MemoryBlock.Count - 1; i++)
-                {
-                    if (MemoryBlock[i] == 0) // Start of a free block
-                    {
-                        int j;
-                        for (j = i; j < MemoryBlock.Count - 1; j++)
-                        {
-                            if (MemoryBlock[j] == 1) // End of a free block
-                            {
-                                break;
-                            }
-                        }
-                        int size = j - i;
-                        if (size >= processSize)
-                        {
-                            bestIndex = i;
-                            break; // Found the first suitable free block
-                        }
-                        i = j; // Skip to the end of the current free block
-                    }
-                }
-                Console.WriteLine($"Reg. Base = {bestIndex}\tReg. Limite = {bestIndex + processSize}\nTamanho Processo: {processSize}");
-                if (bestIndex != -1)
-                {
-                    // Allocate the process to the first free block
-                    for (int i = bestIndex; i < bestIndex + processSize; i++)
-                    {
-                        MemoryBlock[i] = 1;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Memória insuficiente para conter o processo.");
-                }
+                Console.WriteLine();
             }
         }
-
 
     }
 }
--- MemoryManagement/VariableManage.cs
+++ MemoryManagement/VariableManage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection.PortableExecutable;
 
 
@@ -10,25 +11,28 @@ namespace MemoryManagement
             while (true)
             {
                 Console.WriteLine("Escolha uma opção:");
-                Console.WriteLine("0 = Sair | 1 = Alterar bloco | 2 = Alterar processo | 3 = Mostrar blocos | 4 = Mostrar processos");
+                Console.WriteLine("0 = Sair | 1 = Alterar bloco | 2 = Mostrar blocos| 3 = Adicionar um processo |  " +
+                                  "4 = Alterar processo | 5 = Mostrar processos");
                 int opcao = Convert.ToInt32(Console.ReadLine());
                 switch (opcao){
                     case 1:
                         AlteraBloco();
                         break;
                     case 2:
-                        AlteraProcesso();
-                        break;
-                    case 3:
                         MostraBlocos();
                         break;
+                    case 3:
+                        AdicionaProcesso();
+                        break;
                     case 4:
+                        AlteraProcesso();
+                        break;
+                    case 5:
                         MostraProcessos();
                         break;
                     case 0:
-                        return;
                     default:
-                        Console.WriteLine("Sério? Te dei as opções bonitinhas.\nVou considerar que você clicou errado sem querer e quer sair. >:(");
+                        Console.Clear();
                         return;
                 }
             }
@@ -65,6 +69,15 @@ namespace MemoryManagement
             MemoryManage.ProcessSize[processo - 1] = tamanho;
         }
 
+        private static void AdicionaProcesso()
+        {
+            Console.WriteLine("Qual o novo tamanho do processo? Escreva 0 para voltar");
+            int tamanho = Convert.ToInt32(Console.ReadLine());
+            if (tamanho < 1) return;
+            MemoryManage.ProcessSize.Add(tamanho);
+            MostraProcessos();
+        }
+
         private static void MostraBlocos()
         {
             Console.WriteLine("\n\nBlocos de memória estão organizados da seguinte forma:");
@@ -80,11 +93,11 @@ namespace MemoryManagement
         private static void MostraProcessos()
         {
             Console.WriteLine("Processos a serem incluídos estão organizados da seguinte forma:");
-            for (int i = 0; i < MemoryManage.ProcessLength; i++)
+            for (int i = 0; i < MemoryManage.ProcessSize.Count; i++)
             {
                 Console.WriteLine($"Processo {i+1}:\t{MemoryManage.ProcessSize[i]}");
             }
-            Console.ReadLine();
+            Console.WriteLine("\n\n");
             return;
         }
 
